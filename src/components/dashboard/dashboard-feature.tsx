29d684ce913fb54831d0f47fd9b85ba63cb60e32
MikePawel <radlinski.mikolaj@gmail.com>
2025-04-08 14:40:10 +0200

    fixed bug

--- src/components/dashboard/dashboard-feature.tsx
+++ src/components/dashboard/dashboard-feature.tsx
@@ -17,12 +17,12 @@ export default function DashboardFeature() {
   return (
     <div>
       <AppHero
-        title="GM SUPERTEAM GERMANY!"
+        title="GM SUPERTEAM GERMANY!!!"
         subtitle="Say hi to your new Solana dApp."
       />
       <div className="max-w-xl mx-auto py-6 sm:px-6 lg:px-8 text-center">
         <div className="space-y-2">
-          <p>Here are some helpful links to get you started. '</p>
+          <p>Here are some helpful links to get you started.</p>
           {links.map((link, index) => (
             <div key={index}>
               <a
